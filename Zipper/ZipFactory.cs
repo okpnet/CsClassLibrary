bd1dd183ad3c23d043da1a2dfd7ad604af44a8dc
hayato takahashi <umai-beer@hotmail.co.jp>
2024-05-01 07:16:59 +0900

    ok

--- Zipper/ZipFactory.cs
+++ Zipper/ZipFactory.cs
@@ -9,17 +9,16 @@ namespace Zipper
 {
     public class ZipFactory
     {
-        public byte[] CreateZip(string filename, IReadOnlyDictionary<string, byte[]> source)
+        public byte[] CreateZip(IReadOnlyDictionary<string, byte[]> source)
         {
             using var zipmem = new System.IO.MemoryStream();
             using var zip = new System.IO.Compression.ZipArchive(zipmem, System.IO.Compression.ZipArchiveMode.Create, true);
-            var entry1 = zip.CreateEntry($"unhash_{filename}");
             foreach (var buffer in source)
             {
                 var entry = zip.CreateEntry(buffer.Key);
-                using var zipMem = entry1.Open();
+                using var zipMem = entry.Open();
                 using var bufferMem=new System.IO.MemoryStream(buffer.Value);
-                bufferMem.CopyTo(zipmem);
+                bufferMem.CopyTo(zipMem);
             }
             return zipmem.GetBuffer();
         }
