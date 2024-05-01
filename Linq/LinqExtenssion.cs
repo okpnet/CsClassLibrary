476fe90d97f6ecf42da03f0a5940c9da1983c1be
umai-beer <umai-beer@hotmail.co.jp>
2024-05-01 22:29:24 +0900

    ok

---
+++ Linq/LinqExtenssion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExtenssions
+{
+    public static class LinqExtenssion
+    {
+        /// <summary>
+        /// 重複リストを返す
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<TKey> FindDuplication<T,TKey>(this IEnumerable<T> source, Func<T, TKey> predicate)
+        {
+            return source.GroupBy(predicate).Where(t=>t.Count()>1).Select(t=>t.Key);
+        }
+    }
+}
