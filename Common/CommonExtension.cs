eb17655477147240e8f0f8dc6bfc7806c1b5487e
hayato takahashi <umai-beer@hotmail.co.jp>
2024-08-29 08:55:40 +0900

    ok

--- Common/CommonExtension.cs
+++ Common/CommonExtension.cs
@@ -9,16 +9,28 @@ namespace CommonExtensions
     public static class CommonExtension
     {
         /// <summary>
+        /// キャストした値をOutにセットして､Null/非Nullを返す
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="result">NullのときFalse/非NullのときTrue</param>
+        /// <returns></returns>
+        public static bool IsDeclare<TResult>(this TResult value, out TResult result)
+        {
+            result = value;
+            return result is not null;
+        }
+        /// <summary>
         /// キャストした値をOutにセットして､値を返す
         /// </summary>
         /// <typeparam name="TResult"></typeparam>
         /// <param name="value"></param>
         /// <param name="result"></param>
         /// <returns></returns>
-        public static bool IsDeclare<TResult>(this TResult value, out TResult result)
+        public static TResult IfDeclare<TResult>(this TResult value, out TResult result)
         {
             result = value;
-            return result is not null;
+            return result;
         }
     }
 }
