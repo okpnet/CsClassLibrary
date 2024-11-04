ce2b83c925368bb10d7160969d373df9a79d3b98
hayato <umai-beer@hotmail.co.jp>
2024-11-05 07:31:47 +0900

    ok

---
+++ Linq/EnumrableExtenssion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExtenssions
+{
+    /// <summary>
+    /// Enumerable拡張クラス
+    /// </summary>
+    public static class EnumrableExtenssion
+    {
+        /// <summary>
+        /// IEnumerableを実体化してループする
+        /// </summary>
+        /// <typeparam name="T">IEnumerableが保持している型</typeparam>
+        /// <param name="source">IEnumerable</param>
+        /// <param name="action">ループ処理のデリゲート</param>
+        /// <returns>IEnumerable</returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source,Action<T> action)
+        {
+            foreach(var item in source)action(item);
+            return source;
+        }
+    }
+}
